57e530fe218a1fe5f125358d946efeb74298699e
mango906 <minkb01@gmail.com>
2019-08-03 23:48:20 +0900

    [ADD] swipe animation

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { StyleSheet, View, Text, Platform } from "react-native";
 import { Icon } from "native-base";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createMaterialTopTabNavigator
+} from "react-navigation";
 
 //하단 탭 내용들
 import AddMediaTab from "./AppTabNavigator/AddMediaTab";
@@ -10,13 +14,35 @@ import LikesTab from "./AppTabNavigator/LikesTab";
 import ProfileTab from "./AppTabNavigator/ProfileTab";
 import SearchTab from "./AppTabNavigator/SearchTab";
 
-const AppTabNavigator = createBottomTabNavigator({
-  HomeTab: { screen: HomeTab },
-  SearchTab: { screen: SearchTab },
-  AddMediaTab: { screen: AddMediaTab },
-  LikesTab: { screen: LikesTab },
-  ProfileTab: { screen: ProfileTab }
-});
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    HomeTab: { screen: HomeTab },
+    SearchTab: { screen: SearchTab },
+    AddMediaTab: { screen: AddMediaTab },
+    LikesTab: { screen: LikesTab },
+    ProfileTab: { screen: ProfileTab }
+  },
+  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      style: {
+        ...Platform.select({
+          ios: {
+            backgroundColor: "white"
+          }
+        })
+      },
+      iconStyle: { height: 50 },
+      activeTintColor: "#000",
+      inactiveTintColor: "#d1cece",
+      upperCaseLabel: false,
+      showLabel: false,
+      showIcon: true
+    }
+  }
+);
 
 const AppTabContainet = createAppContainer(AppTabNavigator);
 
